89fb836005b394d78757c43f68ff0d2572f32a9d
muditgarg48 <muditgarg48@gmail.com>
2022-10-17 03:11:07 +0100

    Major Visual and Quality of Life improvements with inclusion of PM 2.5 Levels

--- backend.js
+++ backend.js
@@ -1,8 +1,13 @@
 var app = Vue.createApp({
     data() {
         return {
-            message: '',
-            show: false,
+            api_id: 'aa3551b69f041b72a4094701dbc65f92',
+            user_city: '',
+            lat:'',
+            lon:'',
+            content_show: false,
+            pollution_show: false,
+            error_show: false,
             umbrellaCheck: 'Info not retrieved!!',
             feelsLike: '',
             items: [],
@@ -12,6 +17,24 @@ var app = Vue.createApp({
     methods: {
         created() {
             console.clear()
+        },
+        printLocation() {
+            return this.user_city[0].toUpperCase()+this.user_city.substr(1)+" ("+this.lat+", "+this.lon+")"
+        },
+        posCalc(response) {
+            this.lat = response.coord.lat
+            this.lon = response.coord.lon
+        },
+        responseCheck: async function() {
+            let response = await this.retrieveTodayWeather()
+            if(response.cod == '200') {
+                this.content_show = true
+                this.error_show = false
+                this.compute(response)
+            } else if (response.cod == '404') {
+                this.content_show = false
+                this.error_show = true
+            }
         },
         retrieveImg(weather) {
             let icon = weather.icon
@@ -23,40 +46,69 @@ var app = Vue.createApp({
             let url = "http://openweathermap.org/img/wn/"+icon+"@2x.png"
             return url
         },
-        retrieve: async function() {
-            console.log("inside retrieve function (client side)!!")
-            this.show = true
-            city = this.message
-
+        retrieveTodayWeather: async function() {
             //Fetching the weather data for today !!
-            let res = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=aa3551b69f041b72a4094701dbc65f92")
-            this.response = await res.json() 
-            console.log(this.response)
-
-            // showing today's weather symbol
-            this.weather_icon_url = this.retrieveImg2x(this.response.weather[0])
-
+            let res = await fetch("https://api.openweathermap.org/data/2.5/weather?q="+this.user_city+"&units=metric&APPID="+this.api_id)
+            let response = await res.json() 
+            console.log("Weather JSON:")
+            console.log(response)
+            return response
+        },
+        rainStatus(response) {
             // Rain ????
-            let rain = this.response.rain
+            let rain = response.rain
             if(rain == true) {
                 this.umbrellaCheck = "Carry an umbrella ☔✔️"
             } else {
                 this.umbrellaCheck = "Way to go, no rain ⛅❌"
             }
-
+        },
+        feelsLikeStatus(response) {
             // Feels like ????
-            let feels_like = this.response.main.feels_like
+            let feels_like = response.main.feels_like
             this.feelsLike = "It feels like "+ (feels_like)+"°C"
-            
-            //Fetching the forecast data for 5 days !!
-            let resp = await fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=metric&cnt=&APPID=aa3551b69f041b72a4094701dbc65f92")
-            this.response = await resp.json() 
-            console.log(this.response)
-            this.items = this.response.list
+            if(feels_like<=12) {
+                this.feelsLike = "Pack for COLD. "+ this.feelsLike
+            } else if(feels_like>12 && feels_like<24) {
+                this.feelsLike = "Pack for MILD. "+ this.feelsLike
+            } else if(feels_like>=24) {
+                this.feelsLike = "Pack for HOT. "+ this.feelsLike
+            }
+        },
+        forecastFetch: async function() {
+            //Fetching the forecast data for 4 days !!
+            let resp = await fetch("https://api.openweathermap.org/data/2.5/forecast?q="+this.user_city+"&units=metric&cnt=32&APPID="+this.api_id)
+            let data = await resp.json() 
+            console.log("Forecast JSON:")
+            console.log(data)
+            this.items = data.list
+        },
+        retrievePollution: async function() {
+            let res = await fetch("http://api.openweathermap.org/data/2.5/air_pollution/forecast?lat="+this.lat+"&lon="+this.lon+"&appid="+this.api_id)
+            let data = await res.json()
+            console.log("Pollution JSON:")
+            console.log(data)
+            let list = data.list
+            for(let i=0;i<92;i++) {
+                let pm_lvl = list[i].components.pm2_5
+                if(pm_lvl >= 10) {
+                    this.pollution_show = true
+                }
+            }
+        },
+        compute(response) {
+            this.posCalc(response)
+            this.weather_icon_url = this.retrieveImg2x(response.weather[0])
+            this.rainStatus(response)
+            this.feelsLikeStatus(response)
+            this.forecastFetch()
+            this.retrievePollution()
         },
         clearInput() {
-            this.show = false
-            this.message = ''
+            this.content_show = false
+            this.error_show = false
+            this.pollution_show = false
+            this.user_city = ''
             this.umbrellaCheck= 'Info not retrieved!!'
             this.feelsLike = ''
             this.items = []
